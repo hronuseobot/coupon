c9d375c65ef72d3a6518bd32947eec3b8dbd0627
hronuseobot <41598779+hronuseobot@users.noreply.github.com>
2018-11-25 13:41:09 +0200

    Initial commit

---
+++ src/main/java/com/coupon/IndexController.java
@@ -0,0 +1,18 @@
+package com.coupon;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
+public class IndexController {
+    @GetMapping("/index")
+    public String index(
+            @RequestParam(name="name", required = false, defaultValue = "Coupon Finder") String name,
+            Model model
+    ){
+        model.addAttribute("name",name);
+        return "index";
+    }
+}
